c78dbe6d3f3ca99adf0ccaacf7fa4298008a8d78
Dennis <mwendadennis8779@gmail.com>
2022-11-21 22:53:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+function companyName(){
+    const companyName = Scuber; 
+}
+ let mostProfitableNeighborhood = 'Chelsea';
+function mostProfitableNeighborhood() {
+    
+}
+let companyCeo = 'Susan Smith';
+function companyCeo(){
+    
+}
